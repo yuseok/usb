658d3c455a1d73154f47c5c3ad979af38d9b5fad
ohyuseok <ohyuseok@192.168.0.11>
2014-07-17 16:01:04 +0900

    list oK. onitemclicklistener구현 필요.

--- USBConnection/src/yuseok/usbconnection/PrivateListAdapter.java
+++ USBConnection/src/yuseok/usbconnection/PrivateListAdapter.java
@@ -1,43 +1,50 @@
 package yuseok.usbconnection;
 
+import java.util.ArrayList;
+
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
 public class PrivateListAdapter extends BaseAdapter {
 	private Context context = null;
+	private ArrayList<String> data = null;
 	
 	public PrivateListAdapter(Context context) {
 		super();
 		this.context = context;
-		
 		
 	}
 
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return data.size();
 	}
 
 	@Override
 	public Object getItem(int position) {
 		// TOsDO Auto-generated method stub
-		return null;
-	}
+		return data.get(position);
+		}
 
 	@Override
 	public long getItemId(int position) {
 		// TODO Auto-generated method stub
-		return 0;
+		return position;
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		convertView = inflater.inflate(android.R.layout.simple_list_item_1, null);
 		
-		return null;
+		
+		return convertView;
 	}
 
 }
--- USBConnection/src/yuseok/usbconnection/USBList.java
+++ USBConnection/src/yuseok/usbconnection/USBList.java
@@ -1,39 +1,101 @@
 package yuseok.usbconnection;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
 
-public class USBList extends Activity {
-//	ListView mDeviceList;
-	TextView text;
+public class USBList extends ListActivity {
+	// ListView mDeviceList;
+	private ArrayList<String> list = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.simpletextview);
-		text = (TextView) findViewById(R.id.simpletextview);
 		showUSBDrivers();
-		
-		
-		
-//		mDeviceList = (ListView) findViewById(R.id.deviceList);
-//		mDeviceList.setAdapter(new PrivateListAdapter(this));
-//		showUSBDrivers();
+
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list));
+		getListView().setTextFilterEnabled(true);
+
+		getListView().setOnItemClickListener(listener);
 
 		super.onCreate(savedInstanceState);
 	}
+
+	OnItemClickListener listener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+
+			Toast.makeText(USBList.this, String.valueOf(id), Toast.LENGTH_SHORT)
+					.show();
+			;
+
+		}
+	};
 
 	void showUSBDrivers() {
 
 		UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
 		HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
-		text.append(deviceList.toString());
+
+		Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+		while (deviceIterator.hasNext()) {
+			UsbDevice device = deviceIterator.next();
+
+			StringBuilder str = new StringBuilder();
+			str.append("***DeviceName:");
+			str.append(device.getDeviceName() + "\n***Device id:");
+			str.append(device.getDeviceId() + "\n***Vendor id:");
+			str.append(device.getVendorId());
+			list.add(str.toString());
+			// list.add(device.getDeviceName());
+			// list.add(String.valueOf(device.getDeviceId()));
+			// list.add(String.valueOf(device.getVendorId()));
+			//
+
+		}
+
+	}
+	
+	private class TestAdapter extends BaseAdapter{
 		
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			return null;
+		}
 		
 	}
 
